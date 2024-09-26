df39d06625faed6517a9244b20deaec299d7a28c
johmaru <hatunehimeka@outlook.jp>
2024-09-26 13:57:51 +0900

    Update CMake minimum version requirement to 3.22

--- main.c
+++ main.c
@@ -3,13 +3,18 @@
 #include <unistd.h>
 #include <dirent.h>
 #include "core/log_controller.h"
+#include "log.h"
 
 #ifndef MAX_BUF
 #define MAX_BUF 200
 #endif
 
+char instance_time[20];
+FILE *log_file;
+
 int main(void) {
         int os_value = 0;
+       nowtime_getter(instance_time,sizeof(instance_time));
     #if defined(_WIN32) || defined(_WIN64)
         os_value = 1;
     #elif defined(__linux__)
@@ -27,8 +32,21 @@ int main(void) {
     if(directory_exists("logs") == -1){
         create_directory("logs",os_value);
     }
+    
+    char time_path[MAX_BUF];
+    snprintf(time_path,sizeof(time_path),"logs/%s",instance_time);
+    create_directory(time_path,os_value);
 
-
+    char file_path[MAX_BUF + 10];
+    snprintf(file_path,sizeof(file_path),"%s/log.txt",time_path);
+    FILE *file = fopen(file_path,"w");
+    log_file = file;
+    if(file == NULL){
+        printf("Unable to create file\n");
+        return -1;
+    }
+    fprintf(file,"Log file created successfully\n");
+    fclose(file);
 
     return 0;
 }
